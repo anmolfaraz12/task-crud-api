482998dae8dbc35d9cd767d141e1cd558b7794bf
Anmol Faraz <anmolfara6@gmail.com>
2026-08-13 15:40:17 +0500

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+
 app.get('/',(req, res) => {
-    res.send('Hello, server!');
+    res.json({
+        name: "Task API",
+        version: "1.0",
+        endpoints: ["/tasks"]
+    });
 });
+
+app.get('/health' , (req, res) => {
+    res.json({
+        status: "OK"
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
